4dff36c882886a18d09936a627f4810925f6a1ba
“ReshmaScriptlanes1999” <reshmashinde@scriptlanes.com>
2022-10-11 14:48:52 +0530

    inital commit

---
+++ src/app/multisheet/multisheet.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-multisheet',
+  templateUrl: './multisheet.component.html',
+  styleUrls: ['./multisheet.component.scss']
+})
+export class MultisheetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+openFile(){
+  
+}
+}
